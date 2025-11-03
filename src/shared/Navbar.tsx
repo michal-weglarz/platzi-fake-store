3487307df3f738201e253efda08924550d6bb728
michal-weglarz <morteify@icloud.com>
2025-11-03 01:04:05 +0100

    chore: hide nav items beside login button

--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -15,46 +15,49 @@ function Navbar() {
 					<span className={"md:block hidden"}>Fake Store</span>
 				</Link>
 			</div>
-			<ul className="menu menu-horizontal px-4 flex flex-row justify-between items-center gap-2">
-				<li>
-					<Link to={"/products"}>Products</Link>
-				</li>
-			</ul>
+			{/*<ul className="menu menu-horizontal px-4 flex flex-row justify-between items-center gap-2">*/}
+			{/*	<li>*/}
+			{/*		<Link to={"/products"}>Products</Link>*/}
+			{/*	</li>*/}
+			{/*</ul>*/}
 
 			{auth.user == null ? (
 				<Link to={"/login"} className={"btn"}>
 					Login
 				</Link>
 			) : (
-				<div className="dropdown dropdown-end">
-					<div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-						<div className="w-10 rounded-full">
-							<img alt="Tailwind CSS Navbar component" src={auth.user.avatar} />
+				<div className={"flex flex-row gap-2 items-center"}>
+					<p>{auth.user.name}</p>
+					<div className="dropdown dropdown-end">
+						<div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+							<div className="w-10 rounded-full">
+								<img alt="Tailwind CSS Navbar component" src={auth.user.avatar} />
+							</div>
 						</div>
+						<ul
+							tabIndex={-1}
+							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+						>
+							<div className="p-2 flex flex-col gap-2">
+								<p className={"uppercase text-xs badge-xs badge-soft badge-secondary badge"}>
+									{auth.user.role} #{auth.user.id}
+								</p>
+								<p className={"font-bold"}>{auth.user.name}</p>
+								<p className={"text-xs text-slate-600"}>{auth.user.email}</p>
+							</div>
+							<li>
+								<button
+									className={"btn btn-neutral btn-block btn-sm"}
+									onClick={() => {
+										api.auth.logout();
+										queryClient.resetQueries({ queryKey: ["profile"] });
+									}}
+								>
+									Log out
+								</button>
+							</li>
+						</ul>
 					</div>
-					<ul
-						tabIndex={-1}
-						className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-					>
-						<div className="p-2 flex flex-col gap-2">
-							<p className={"uppercase text-xs badge-xs badge-soft badge-secondary badge"}>
-								{auth.user.role} #{auth.user.id}
-							</p>
-							<p className={"font-bold"}>{auth.user.name}</p>
-							<p className={"text-xs text-slate-600"}>{auth.user.email}</p>
-						</div>
-						<li>
-							<button
-								className={"btn btn-neutral btn-block btn-sm"}
-								onClick={() => {
-									api.auth.logout();
-									queryClient.resetQueries({ queryKey: ["profile"] });
-								}}
-							>
-								Log out
-							</button>
-						</li>
-					</ul>
 				</div>
 			)}
 		</nav>
